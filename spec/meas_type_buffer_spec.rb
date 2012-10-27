f5995c06ea5bbed94bc660a9bf03cf11d4ad31e9
Aleksander Kwiatkowski <bobikx@poczta.fm>
2012-10-27 19:56:42 +0200

    meas type buffer

--- spec/meas_type_buffer_spec.rb
+++ spec/meas_type_buffer_spec.rb
@@ -1,7 +1,20 @@
 require 'spec_helper'
 
 describe MeasReceiver::MeasTypeBuffer do
-  it "simple" do
-    true.should
+  before :each do
+    mc = {
+      name: 'u_batt',
+      unit: 'V',
+      fetch_interval: 0.2,
+      command: ['0'],
+      response_size: 2,
+    }
+
+    @m = MeasReceiver::MeasTypeReceiver.new(mc)
+    @b = @m.meas_buffer
+  end
+
+  it "simple stuff" do
+    @b.should be_kind_of(MeasReceiver::MeasTypeBuffer)
   end
 end
